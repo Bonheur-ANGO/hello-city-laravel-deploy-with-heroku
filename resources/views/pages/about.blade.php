13a4d9c75e87564fd40f564847c33cf0369a2a75
Bonheur ANGO <angomillenium@gmail.com>
2021-05-26 13:38:10 +0200

    Remove duplication By using layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <title>Hello City</title>
+<p>Built with &hearts; by Bonheur ANGO</p>
+<p> <a href="/">Revenir à la page d'accueil</a> </p>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body>
-    <p>Built with &hearts; by Bonheur ANGO</p>
-    <p> <a href="/">Revenir à la page d'accueil</a> </p>
-
-
-
-    <footer>
-        <p>&copy; Copyrights {{ date('Y') }}</p>
-    </footer>
-</body>
-
-</html>
+@endsection
